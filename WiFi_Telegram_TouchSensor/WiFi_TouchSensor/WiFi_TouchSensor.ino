c7d8a71f60e5245bc2de090e014c668e499fbd17
danprav04 <62667601+danprav04@users.noreply.github.com>
2021-12-12 17:05:28 +0200

    Update WiFi_TouchSensor.ino
    Mid Work
    Added - Multi Tasking

--- WiFi_Telegram_TouchSensor/WiFi_TouchSensor/WiFi_TouchSensor.ino
+++ WiFi_Telegram_TouchSensor/WiFi_TouchSensor/WiFi_TouchSensor.ino
@@ -47,14 +47,53 @@ void setup() {
  
   Serial.println("Connected to the WiFi network");
   Serial.println(WiFi.localIP());
+
+  xTaskCreatePinnedToCore(
+    TelegramCheck,      // Function that should be called
+    "TelegramCheck",    // Name of the task (for debugging)
+    2048,               // Stack size (bytes)
+    NULL,               // Parameter to pass
+    1,                  // Task priority
+    NULL,               // Task handle
+    1          // Core you want to run the task on (0 or 1)
+);
   
   bot.sendMessage(CHAT_ID, "Bot started up", "");
 }
 
 int count1 = 0;
 
+void TelegramCheck(void * parameter){
+  for(;;){
+//       int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+//  
+//      while(numNewMessages) {
+//        //Serial.println("got response");
+//        handleNewMessages(numNewMessages);
+//        numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+//      }
+        
+      delay(10);
+
+      if((digitalRead(12) == HIGH)){
+        count1+=1;
+//        Serial.print(count1); Serial.print(" / "); Serial.println(delayBetweanAlerts);
+//        if(count1 > delayBetweanAlerts){
+//          Serial.println("Alert");
+//          noTouchAlert();
+//          count1 = 0;
+//        }
+      }
+      else
+        count1 = 0;
+  }
+  
+    vTaskDelete(NULL);
+}
+
+
 void loop() {
-  if(!isTouch){
+//  if(!isTouch){
     int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
 
     while(numNewMessages) {
@@ -62,7 +101,7 @@ void loop() {
       handleNewMessages(numNewMessages);
       numNewMessages = bot.getUpdates(bot.last_message_received + 1);
     }
-  }
+//  }
     
   
       if((digitalRead(12) == HIGH) && isTouch){
@@ -81,7 +120,7 @@ void loop() {
       delay(10);
 
       if((digitalRead(12) == HIGH)){
-        count1+=1;
+//        count1+=1;
         Serial.print(count1); Serial.print(" / "); Serial.println(delayBetweanAlerts);
         if(count1 > delayBetweanAlerts){
           Serial.println("Alert");
@@ -123,14 +162,16 @@ void handleNewMessages(int numNewMessages) {
 
     String from_name = bot.messages[i].from_name;
     
-    if (text.indexOf("/AlertSet") >= 0) {
+    if (text.indexOf("/alertset") >= 0) {
       text.remove(0, 10);
       delayBetweanAlerts = text.toInt();
       bot.sendMessage(chat_id, "Done", "");
     }
-    if (text.indexOf("/Alert") >= 0) {
+    if (text.indexOf("/alert") >= 0) {
       noTouchAlert();
       bot.sendMessage(chat_id, "Done", "");
     }
+    else
+     bot.sendMessage(chat_id, "I can't understand you.", "");
   }
 }
